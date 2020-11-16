f7ba50fe058b2532f38a43b11d51aee96adffe18
PeterBurgs <peterburgs.vn@gmail.com>
2020-11-17 01:11:41 +0700

    Bug while setting init region

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,13 +1,21 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import "../_mockLocation";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
 import Map from "../components/Map";
+import { Context as LocationContext } from "../context/LocationContext";
+import useLocation from "../hooks/useLocation";
 
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
+  const [err] = useLocation(addLocation);
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
-      <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>
+      <Text h2>Create a Track</Text>
       <Map />
+      {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   );
 };
